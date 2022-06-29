34b15c412aaf888a4adf3469877f5af1b25c89d9
annette BWEMERE <bwemereannette@gmail.com>
2022-06-29 16:21:48 +0200

    feat:ajout du timer javascript

--- js/index.js
+++ js/index.js
@@ -419,3 +419,20 @@ mesInputs.forEach((el) => {
 mesSuivants.forEach((but) => {
     but.addEventListener('click', () => mesSuivants.forEach((but) => but.disabled = true))
 })
+
+const departMinutes = 1.03;
+let temps = departMinutes * 60;
+
+const timerElement = document.querySelector("#timer");
+
+setInterval(() => {
+    let minutes = parseInt(temps / 60, 10);
+    let secondes = parseInt(temps % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+
+    timerElement.innerText = `${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+
+}, 1000)
