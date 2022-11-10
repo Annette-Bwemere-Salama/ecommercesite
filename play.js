9fb17a406747b926868c25a4af4a1bfb006574b6
annette Bwemere <bwemereannette@gmail.com>
2022-11-10 15:24:05 +0200

    feat: ajout des destrictiring

--- play.js
+++ play.js
@@ -1,8 +1,35 @@
 const person = {
     name: "Annette",
     age: 29,
-    greet ()  {
+    greet() {
         console.log("Hi, I am " + this.name);
     }
 };
-person.greet();
+
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person)
+
+const { name, age } = person;
+console.log(name, age);
+// const copiedPerson = { ...person };
+// console.log(copiedPerson) ;
+// const hobbies = ["Sports", "Cooking"];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby => 'Hobby' + hobby
+// ));
+// console.log(hobbies);
+
+// const copieArray = [...hobbies];
+// console.log(copieArray);
+
+// const toArray = (...arg) =>{
+//  return arg;
+// }
+
+// console.log(toArray(1,2,3,4));
