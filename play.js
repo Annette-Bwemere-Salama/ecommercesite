7de93521f4548c85e71f7f3fd5eaa2f15c58ad71
annette Bwemere <bwemereannette@gmail.com>
2022-11-10 14:40:17 +0200

    first-commit

---
+++ play.js
@@ -0,0 +1,14 @@
+const name = 'Max';
+const age = 29;
+const hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHabb) {
+    return(
+        "Name is" + 
+        userName + 'age is' + 
+        userAge + 
+        userHabb
+    );
+}
+
+console.logsummarizeUser(name, age, hasHobbies);
