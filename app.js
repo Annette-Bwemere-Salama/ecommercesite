6e0d87e0c92cb781f096622a888337a6a2ba180f
annette Bwemere <bwemereannette@gmail.com>
2022-11-30 14:46:07 +0200

    feat: ajout fs

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const http = require('http');
 
+const fs = require('fs')
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url
+
+    if (url === '/') {
+        res.write("<html>");
+        res.write("<head><title>Entrez Message</title><head>")
+        res.write("<body><form action='/message' method='POST'><input type='test' name='message'><button type={submit}>Send</button></form></body>");
+        res.write("<body><form><input ></form></body>")
+        res.write("</html>")
+        return res.end();
+    }
+    // console.log(req.url, req.method, req.headers);
+    if (url === '/message' && method === 'POST') {
+        fs.writeFile('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
-    res.write("<head><title>My First Page</title></head>")
-    res._write("<body><h1>Hello From my Node.js Server!</h1></body>")
+    res.write("<html>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write("<body><h1>Hello From my Node.js Server!</h1></body>");
     res.write("</html>")
     res.end();
 });
