ce0d342884fe065e7602bf67424f3daf14481b8b
annette Bwemere <bwemereannette@gmail.com>
2022-11-30 15:02:49 +0200

    feat: ajout du fichier msg

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const http = require('http');
 const fs = require('fs')
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method;
 
     if (url === '/') {
         res.write("<html>");
         res.write("<head><title>Entrez Message</title><head>")
-        res.write("<body><form action='/message' method='POST'><input type='test' name='message'><button type={submit}>Send</button></form></body>");
+        res.write("<body><form action='/message' method='POST'><input type='test' name='message'><button type='submit'>Send</button></form></body>");
         res.write("<body><form><input ></form></body>")
         res.write("</html>")
         return res.end();
     }
     // console.log(req.url, req.method, req.headers);
     if (url === '/message' && method === 'POST') {
-        fs.writeFile('message.txt', 'DUMMY');
+        fs.writeFileSync('message.txt', 'DUMMY');
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
