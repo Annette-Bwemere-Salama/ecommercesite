059efa0bf010340f9f5e41e42173842b7e0bd0d2
annette Bwemere <bwemereannette@gmail.com>
2022-11-30 20:39:30 +0200

    feat: module export

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
+console.log(routes.someText);
 const routes = require('./routes.js')
 
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 server.listen(3000);
 
--- routes.js
+++ routes.js
@@ -37,5 +37,15 @@ const requestHandler = (req, res) => {
     res.end();
 
 };
+// module.exports = requestHandler
 
-module.exports = requestHandler;
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some hard coded text'
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text'
