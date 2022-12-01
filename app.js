b31bad115f600fc829dbc748521621f882f06d8d
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 14:28:19 +0200

    feat: add express-routes

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const app = express();
 
 
 app.use('/', (req, res, next) => {
+    console.log("This always runs");
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
     console.log("In the middleware");
+    res.send('<h1>Product!</h1>')
     next();
 });
 
