a5cc9f9f0673f3f8c2b83f4f8e88adbe307f0a56
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 14:45:10 +0200

    feat: add express-routes-differentes

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 
-const express = require('express')
+const express = require('express');
+const { text } = require('stream/consumers');
 
 const app = express();
 
 
-app.use('/', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("This always runs");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add-Product</button></form/>')
     next();
 })
 
-app.use('/add-product', (req, res, next) => {
+app.use('/product', (req, res, next) => {
     console.log("In the middleware");
-    res.send('<h1>Product!</h1>')
+    console.log(req.body);
+    // res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add-Product</button></form/>')
+    res.redirect('/')
     next();
 });
 
-
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from expressAnny!</h1>')
+app.use('/', (req, res, next) => {
+    console.log(req.body);
+    res.send('<h1>Hello from express!</h1>')
 })
+
 app.listen(3000);
 
