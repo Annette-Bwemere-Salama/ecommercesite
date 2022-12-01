93b45acebba67fad54343605b547d5434557cc4e
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 15:15:12 +0200

    feat: add express-add post and get

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser')
 const app = express();
 
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add-Product</button></form/>')
 })
 
-app.use('/product', (req, res, next) => {
-    console.log("In the middleware");
-    console.log(req.body);
+app.post('/product', (req, res, next) => {
+
     res.redirect('/');
 });
 
