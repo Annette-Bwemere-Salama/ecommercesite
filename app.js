f4022c4059f2ec7283f1a39f24a98dbd25deaf17
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 14:25:16 +0200

    feat: add express

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http = require('http');
 
-// console.log(routes.someText);
+const express = require('express')
 
-const routes = require('./routes')
+const app = express();
 
-const server = http.createServer(routes.handler);
-server.listen(3000);
+
+app.use('/', (req, res, next) => {
+    console.log("In the middleware");
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from expressAnny!</h1>')
+})
+app.listen(3000);
 
