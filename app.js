fa7e5c7abcf63211cad475f54e41dd81f689fb56
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 16:03:13 +0200

    feat: add express-add page error 404

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.use(shopRoutes);
 
 
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express!</h1>')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Foud</h1>')
 });
+
+
 
 app.listen(3000);
 
