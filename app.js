9d8d177c8e9f797db4d0f15603c00b59ed780828
annette Bwemere <bwemereannette@gmail.com>
2022-12-01 14:53:04 +0200

    feat: add express-add body parser

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 
 const express = require('express');
-const { text } = require('stream/consumers');
 
+const bodyParser = require('body-parser')
 const app = express();
 
 
+app.use(bodyParser.urlencoded());
 app.use('/add-product', (req, res, next) => {
-    console.log("This always runs");
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add-Product</button></form/>')
-    next();
 })
 
 app.use('/product', (req, res, next) => {
     console.log("In the middleware");
     console.log(req.body);
-    // res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add-Product</button></form/>')
-    res.redirect('/')
-    next();
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log(req.body);
     res.send('<h1>Hello from express!</h1>')
-})
+});
 
 app.listen(3000);
 
