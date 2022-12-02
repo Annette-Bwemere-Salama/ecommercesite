b253d8d014f6dee8ca838fbd12ba0dfbdbe7c242
annette Bwemere <bwemereannette@gmail.com>
2022-12-02 02:55:32 +0200

    feat: rootDiirector

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path')
 const router = express.Router();
 
+const rooDir = require('../util/path')
 
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rooDir, 'views', 'add-product.html'))
 });
 
 
